f4014e952fd67a1d594923c8d9b4a74007f4c389
kartavik <roman.varkuta@gmail.com>
2019-04-24 18:10:55 +0300

    Add base simple preloader style

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/main.css';
+import './styles/preloader.css';
 import './images/user_default.png';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
