b1e9eceb6bd889a2d0c5a2d05386e79a3cbb29e7
Roman Varkuta <roman.varkuta@gmail.com>
2019-11-17 00:21:37 +0200

    Refactor (#1)

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {Col, Container, Row, Nav} from 'react-bootstrap';
 import {Route} from "react-router";
 import Console from './Dashboard/Console';
 import Settings from "./Dashboard/Settings";
@@ -9,25 +8,25 @@ import {BrowserRouter as Router, Link} from "react-router-dom";
 
 export default class Dashboard extends React.Component {
     render() {
-        return <Container fluid className={"pt-2"}>
-            <Row>
+        return <div className="container-fluid pt-2">
+            <div className="row">
                 <Router>
-                    <Col md={2}>
-                        <Nav defaultActiveKey="/console" className="flex-column">
-                            <Link to="console" className={"bg-lightgrey-hover text-secondary m-1 nav-link"}>Console</Link>
-                            <Link to="/settings" className={"bg-lightgrey-hover text-secondary m-1 nav-link"}>Setting</Link>
-                            <Link to="/statistics" className={"bg-lightgrey-hover text-secondary m-1 nav-link"}>Statistics</Link>
-                            <Link to="/users" className={"bg-lightgrey-hover text-secondary m-1 nav-link"}>Users</Link>
-                        </Nav>
-                    </Col>
-                    <Col md={10}>
+                    <div className="col-md-2">
+                        <div className="nav flex-column">
+                            <Link to="/console" className="bg-lightgrey-hover text-secondary m-1 nav-link">Console</Link>
+                            <Link to="/settings" className="bg-lightgrey-hover text-secondary m-1 nav-link">Setting</Link>
+                            <Link to="/statistics" className="bg-lightgrey-hover text-secondary m-1 nav-link">Statistics</Link>
+                            <Link to="/users" className="bg-lightgrey-hover text-secondary m-1 nav-link">Users</Link>
+                        </div>
+                    </div>
+                    <div className="col-md-10">
                         <Route path={"/console"} component={Console}/>
                         <Route path={"/settings"} component={Settings}/>
                         <Route path={"/statistics"} component={Statistics}/>
                         <Route path={"/users"} component={Users}/>
-                    </Col>
+                    </div>
                 </Router>
-            </Row>
-        </Container>
+            </div>
+        </div>
     }
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,25 @@
 import * as React from "react";
-import {Nav, Navbar} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 export default class Header extends React.Component {
     public render() {
-        return (
-            <Navbar variant={"dark"} className="bg-primary shadow">
-                <Nav>
-                    <Link to="/" className="nav-link">Home</Link>
-                </Nav>
-                <Nav>
-                    <Link to="/profile" className="nav-link">Profile</Link>
-                </Nav>
-                <Nav>
-                    <Link to="/statistics" className="nav-link">Statistics</Link>
-                </Nav>
-            </Navbar>
-        );
+        return  <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
+            <Link className="navbar-brand" to="/">Brand</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbar-menu" aria-controls="navbar-menu"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"/>
+            </button>
+            <div className="collapse navbar-collapse" id="navbar-menu">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/profile">Profile</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/statistics">Statistics</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>;
     }
 }
